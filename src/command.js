818d6d9e92453f58ea26e5eb1bf2fc43d6d4fc8d
Nir Elbaz <nire0510@gmail.com>
2017-06-20 22:33:56 +0300

    append .unpacked to jq path

--- src/command.js
+++ src/command.js
@@ -5,7 +5,7 @@ const JQ_PATH = path.join(__dirname, '..', 'bin', 'jq')
 
 export const commandFactory = (filter, json, options = {}) => {
   return {
-    command: JQ_PATH,
+    command: JQ_PATH.replace('/app.asar/', '/app.asar.unpacked/'),
     args: parseOptions(filter, json, options)
   }
 }
